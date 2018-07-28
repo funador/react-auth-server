1ba7d0b2befa1caaa2b0ce9c2723d49e1a874f0c
funador <jesse.heaslip@gmail.com>
2018-07-28 13:52:44 -0500

    updating comments

--- server.js
+++ server.js
@@ -9,14 +9,17 @@ const session = require('express-session')
 const cors = require('cors')
 const socketio = require('socket.io')
 const authRouter = require('./lib/auth.router')
-const { passportInit } = require('./lib/passport.init')
+const passportInit = require('./lib/passport.init')
 const { SESSION_SECRET, CLIENT_ORIGIN } = require('./config')
 const app = express()
 let server
 
+// If we are in production we are already running in https
 if (process.env.NODE_ENV === 'production') {
   server = http.createServer(app)
 }
+// We are not in production so load up our certificates to be able to 
+// run the server in https
 else {
   const certOptions = {
     key: fs.readFileSync(path.resolve('certs/server.key')),
@@ -25,31 +28,34 @@ else {
   server = https.createServer(certOptions, app)
 }
 
+// Setup for passport and to accept JSON objects
 app.use(express.json())
 app.use(passport.initialize())
 passportInit(passport)
 
+// Accept requests from our client
 app.use(cors({
   origin: CLIENT_ORIGIN
 })) 
 
+// saveUninitialized: true allows us to attach the socket id to the session
+// before we have athenticated the user
 app.use(session({ 
   secret: process.env.SESSION_SECRET, 
   resave: true, 
   saveUninitialized: true 
 }))
 
+// Connecting sockets to the server and adding them to the request 
+// so that we can access them later in the controller
 const io = socketio(server)
 app.set('socketio', io)
 
-io.sockets.on('connection', socket => {
-  socket.on('auth', provider => {   
-    socket.join(provider)
-  })
-})
+// Catch a start up request so that a sleepy Heroku instance can  
+// be responsive as soon as possible
+app.get('/wake-up', (req, res) => res.send('👍'))
 
-app.get('/wake-up-heroku', (req, res) => res.end())
-
+// Direct all other requests at our auth router
 app.use('/', authRouter)
 
 server.listen(process.env.PORT || 8080, () => {
